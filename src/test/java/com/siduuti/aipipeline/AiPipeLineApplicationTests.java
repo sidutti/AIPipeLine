0db38fdd6568f286da465ebb6ad47e2674f98595
Harish Sankaran <sharish.sankaran@gmail.com>
2025-04-13 09:10:28 -0700

    init

---
+++ src/test/java/com/siduuti/aipipeline/AiPipeLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siduuti.aipipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPipeLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
